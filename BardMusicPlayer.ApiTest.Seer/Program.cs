c8de672711b64ebbaf843f8475da8e1b4038feea
MoogleTroupe <ffxivmusicbard@gmail.com>
2021-05-13 00:23:08 -0400

    rename project

--- BardMusicPlayer.ApiTest.Seer/Program.cs
+++ BardMusicPlayer.ApiTest.Seer/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
- * Copyright(c) 2021 MoogleTroupe, 2018-2020 parulina
- * Licensed under the GPL v3 license. See https://github.com/BardMusicPlayer/BardMusicPlayer/blob/develop/LICENSE for full license information.
+ * Copyright(c) 2021 MoogleTroupe
+ * Licensed under the GPL v3 license. See https://github.com/BardMusicPlayer/BardMusicPlayer.ApiTest/blob/develop/LICENSE for full license information.
  */
 
 using System;
